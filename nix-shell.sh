c33ece1b1fc77972e219c04475273e0e0cd5ac57
Andrew Abbott <andrew@mirx.dev>
2022-04-26 16:53:26 +1000

    DO NOT INCLUDE: Update nix-shell.sh for ROCK64

--- nix-shell.sh
+++ nix-shell.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-ATF_RK3399=$(nix-build '<nixpkgs>' -A pkgsCross.aarch64-multiplatform.armTrustedFirmwareRK3399 --no-out-link)
+ATF_RK3328=$(nix-build '<nixpkgs>' -A pkgsCross.aarch64-multiplatform.armTrustedFirmwareRK3328 --no-out-link)
 
-export DTC=dtc CROSS_COMPILE=aarch64-unknown-linux-gnu- BL31=$ATF_RK3399
+export DTC=dtc CROSS_COMPILE=aarch64-unknown-linux-gnu- BL31=$ATF_RK3328
 export BINMAN_DEBUG=1 BINMAN_VERBOSE=5
 
 echo Now you can run:
 echo "  patchPhase"
-echo "  make rockpro64-rk3399_defconfig"
+echo "  make rock64-rk3328_defconfig"
 echo "  make"
 
-nix-shell '<nixpkgs>' -A pkgsCross.aarch64-multiplatform.ubootRockPro64
+nix-shell '<nixpkgs>' -A pkgsCross.aarch64-multiplatform.ubootRock64
